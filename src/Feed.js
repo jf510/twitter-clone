7e203af57dd43a56eb44d85fba0b5a6a980135e6
Jfontaine <jfontaine19@gmail.com>
2022-05-16 14:11:22 -0400

    imported Tweetbox.js

---
+++ src/Feed.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Feed.css";
+import TweetBox from "./TweetBox";
+
+function Feed() {
+  return (
+    <div className="feed">
+      {/** HEADER */}
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      {/** TWEETBOX */}
+      <TweetBox />
+      {/** POST */}
+
+      {/** POST */}
+      {/** POST */}
+      {/** POST */}
+      {/** POST */}
+    </div>
+  );
+}
+
+export default Feed;
