714f977cc9c4284b89d8e635dfb5a7eba622c407
Jfontaine <jfontaine19@gmail.com>
2022-05-16 15:36:30 -0400

    added post function with props

---
+++ src/Post.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Post.css";
+
+function Post({
+  displayName,
+  username,
+  verified,
+  text,
+  image,
+  avatar,
+  timestamp,
+}) {
+    return <div className="post">
+      
+  </div>;
+}
+
+export default Post;
