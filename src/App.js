fb3b8455e5b48d66905562c21dbd59e04ce6ebdc
Jfontaine <jfontaine19@gmail.com>
2022-05-16 20:51:22 -0400

    rendered widgets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter App</h1>
-
       {/** Sidebar */}
       <Sidebar />
+
       {/** Feed */}
+      <Feed />
+
       {/** Widgets */}
+      <Widgets />
     </div>
   );
 }
