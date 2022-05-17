b0ce78569931982fe29ca154e660220772475df3
Jfontaine <jfontaine19@gmail.com>
2022-05-17 10:28:39 -0400

    created login page with input fields

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,47 @@
-import React from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import FlutterDashIcon from "@mui/icons-material/FlutterDash";
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className="login">
       <div className="login__header">
         <Link to={"/home"}>
           <FlutterDashIcon className="login__twitterIcon" />
         </Link>
-        <h2> Welcome to Warbler</h2>
+        <h1> Welcome to Warbler</h1>
       </div>
-      <div className="login__form">
+      <div className="login__container">
+        <h3>Sign-In</h3>
         <form>
-          <h3>Enter Username</h3>
-          <input type='text' placeholder='Enter username'></input>
-          
+          <h5>Email</h5>
+          <input
+            
+            type="email"
+            placeholder="Enter username"
+          ></input>
+
+          <h5>Password</h5>
+          <input
+            
+            type="password"
+            placeholder="Enter username"
+          ></input>
+
+          <button type="submit" className="login__signinButton">
+            Sign In
+          </button>
         </form>
+        <p>
+          By signing in you agree to the Warbler Terms of Conduct and Privacy.
+        </p>
+        <button type="submit" className="login__signupButton">
+          Create Account
+        </button>
       </div>
     </div>
   );
