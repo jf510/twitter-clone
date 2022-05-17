3f30de69301d345ae7b3ef2e88dda555c58d8a1c
Jfontaine <jfontaine19@gmail.com>
2022-05-16 23:29:40 -0400

    passed props into post. pulled data from firebase database

--- src/Post.js
+++ src/Post.js
@@ -19,34 +19,32 @@ function Post({
   return (
     <div className="post">
       <div className="post__avatar">
-        <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTyD3Ogo63d0zlybYFL5o6Pgf11j3JZtpgaA&usqp=CAU" />
+        <Avatar src={avatar} />
       </div>
       <div className="post__body">
         <div className="post__header">
           <div className="post__headerText">
             <h3>
-              SomeBot2398
+              {displayName}
               <span className="post__headerSpecial">
-                <VerifiedIcon className="post__badge" /> @SomeBot
+                {verified && <VerifiedIcon className="post__badge" />} @
+                {username}
               </span>
             </h3>
           </div>
-          <div className="post__headerDescription"></div>
+          <div className="post__headerDescription">
+            <p>{text}</p>
+          </div>
         </div>
-        <img
-          src="https://c.tenor.com/Zln2iys63zwAAAAM/thats-my-opinion-ashwin-ganesh.gif"
-          alt=""
-        />
+        <img src={image} alt="" />
         <div className="post__footer">
           <ChatBubbleOutlineIcon fontSize="small" />
           <RepeatIcon fontSize="small" />
           <FavoriteBorderIcon fontSize="small" />
           <PublishIcon fontSize="small" />
         </div>
-          </div>
       </div>
-      
-      
+    </div>
   );
 }
 
