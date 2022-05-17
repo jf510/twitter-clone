737d2e12b406ab694707378b70a0a58ca1d1ae30
Jfontaine <jfontaine19@gmail.com>
2022-05-16 21:14:00 -0400

    updating tweet box avatar

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TweetBox.css";
+import Feed from "./Feed";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
 
@@ -9,13 +10,14 @@ function TweetBox() {
       <form>
         <div className="tweetbox__input">
           <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTyD3Ogo63d0zlybYFL5o6Pgf11j3JZtpgaA&usqp=CAU" />
-          <input placeholder="What's happening?"></input>
+          <input type="text" placeholder="What's happening?"></input>
         </div>
         <input
+          type="text"
           className="tweetbox__imageInput"
           placeholder="Add image url"
         ></input>
-        <Button className="tweetbox__Button">Tweet</Button>
+        <Button className="tweetbox__Button">Quack</Button>
       </form>
     </div>
   );
