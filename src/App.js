ce282e095a9dac6ada89fcce3ea971bf7446d2a1
Jfontaine <jfontaine19@gmail.com>
2022-05-17 10:47:50 -0400

    imported browser router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import "./App.css";
-import Sidebar from "./Sidebar";
-import Feed from "./Feed";
-import Widgets from "./Widgets";
+import Login from "./Login";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      {/** Sidebar */}
-      <Sidebar />
-
-      {/** Feed */}
-      <Feed />
-
-      {/** Widgets */}
-      <Widgets />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/home" element={[<Home />]}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
